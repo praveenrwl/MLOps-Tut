1876af1359b938b8efd58f015ed4fe3a9fb6efe6
praveenrwl <praveenpalrawal@gmail.com>
2025-07-13 16:18:12 +0530

    Project - basic structure ready

--- OOPs/oops_proj.py
+++ OOPs/oops_proj.py
@@ -6,12 +6,12 @@ class chatbot:
         self.menu()
 
     def menu(self):
-        user_input = input('Welcome to Chatbot !! How would you like to proceed' \
-        '1. Press 1 to SignUp!!' \
-        '2. Press 2 to SignIn' \
-        '3. Press 3 to write a post' \
-        '4. Press 4 to message a friend' \
-        '5. Press any other key to exit')
+        user_input = input('''Welcome to Chatbot !! How would you like to proceed?
+        1. Press 1 to SignUp!!
+        2. Press 2 to SignIn
+        3. Press 3 to write a post
+        4. Press 4 to message a friend
+        5. Press any other key to exit''')
 
         if user_input == '1':
             pass
@@ -23,5 +23,5 @@ class chatbot:
             pass
         else:
             exit()
-        
+         
 obj = chatbot()
